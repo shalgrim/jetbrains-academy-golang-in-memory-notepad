58bea1dcf82cf0ad7b7b05cf010adaf5e320dd21
Scott Halgrim <shalgrim@gmail.com>
2023-11-22 10:21:14 -0600

    stage 1 complete

--- In-Memory Notepad/task/main.go
+++ In-Memory Notepad/task/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
 func main() {
-	// 	write your code here
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Println("Enter a command and data:")
+		scanner.Scan()
+		input := strings.Split(scanner.Text(), " ")
+		command, data := input[0], strings.Join(input[1:], " ")
+		if command == "exit" {
+			fmt.Println("[Info] Bye!")
+			os.Exit(0)
+		}
+		fmt.Printf("%s %s\n", command, data)
+	}
 }
