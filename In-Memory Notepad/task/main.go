d986533cc1cffe6843a2a9c42355ea74b5a36d27
Scott Halgrim <shalgrim@gmail.com>
2023-11-22 10:46:54 -0600

    stage 2 complete

--- In-Memory Notepad/task/main.go
+++ In-Memory Notepad/task/main.go
@@ -8,17 +8,33 @@ import (
 )
 
 func main() {
+	const MaxNotes = 5
 	scanner := bufio.NewScanner(os.Stdin)
+	notes := make([]string, 0, MaxNotes)
 
 	for {
 		fmt.Println("Enter a command and data:")
 		scanner.Scan()
 		input := strings.Split(scanner.Text(), " ")
 		command, data := input[0], strings.Join(input[1:], " ")
-		if command == "exit" {
+		switch command {
+		case "create":
+			if len(notes) == MaxNotes {
+				fmt.Println("[Error] Notepad is full")
+			} else {
+				notes = append(notes, data)
+				fmt.Println("[OK] The note was successfully created")
+			}
+		case "list":
+			for i, n := range notes {
+				fmt.Printf("[Info] %d: %s\n", i+1, n)
+			}
+		case "clear":
+			notes = make([]string, 0, 5)
+			fmt.Println("[OK] All notes were successfully deleted")
+		case "exit":
 			fmt.Println("[Info] Bye!")
 			os.Exit(0)
 		}
-		fmt.Printf("%s %s\n", command, data)
 	}
 }
